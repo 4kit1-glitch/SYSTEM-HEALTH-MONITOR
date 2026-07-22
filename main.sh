1eb721667bf3f05d215c46c6dfd9a95e7060e1be
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 00:53:08 +0100

    feat: set original layout

--- main.sh
+++ main.sh
@@ -1,3 +1,32 @@
 #!/bin/env bash
 
 # system health monitor
+
+set -euo pipefail
+
+
+#----------------------- GLOBAL VARS--------------------
+
+
+# ---------- GETTERS ---------------------------------
+
+get_storage_info() {
+
+}
+
+get_battery_info() {
+
+}
+
+get_cpu_info() {
+
+}
+
+get_gpu_info() {
+
+}
+
+#----------- INFO PROCESSORS -----------------
+info_processor() {
+    
+}
