ff02f76a203a36b86bcdf470b121be663c0d7c7a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 22:16:31 +0100

    feat: add sudo wrapper and kill permissions functions

---
+++ lib/core/privileged.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# script performs a wrapper to sudo
+
+# gives a process super user privileges
+run_privileged() {
+    if [[ $EUID -eq 0 ]]; then
+        "$@"
+        return $?
+    fi
+    # control user decision
+    read -rp "Process requires super user privilages: proceed with sudo? [y/n]: " response
+    if [[ $response =~ ^[Yy]$ ]]; then 
+        sudo "$@"
+    else 
+        printf "permission denied -- process aborted" >&2
+        return 1
+    fi
+}
+
+# remove sudo privileges
+kill_sudo() {
+    [[ $EUID -eq 0 ]] && sudo -k
+    return 
+}
