660e8b5cf6a0ffc28ad9554691d18eb80ab97f0a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-24 00:07:42 +0100

    fix: remove dublicate pipefail checks

--- lib/memory.sh
+++ lib/memory.sh
@@ -3,22 +3,26 @@
 # script carries functions that work with system memory info and test
 # ext stands for extended
 
-set -euo pipefail
-
 #----------- Getters --------------------------
 
 #---------------RAM-----------------------------
 # quick details about ram 
 get_short_ram_info() {
-    local short_ram_info="$(free -h | sed -n '/Mem/p' | 
-                        awk '{ printf "total: %10s\nused: %11s\navailable: %6s", $2, $3, $7 end}' | 
-                        sed 's/Gi/Gb/')"
+    local short_ram_info="$(
+        free -h | sed -n '/Mem/p' | 
+        awk '{ printf "total: %10s\nused: %11s\navailable: %6s", $2, $3, $7 end}' | 
+        sed 's/Gi/Gb/'
+    )"
     
     printf "%s\n" "$short_ram_info"
 }
 
 # full details about ram
 get_full_ram_info() {
+    local full_ram_info="$(
+
+
+    )"
 
     echo p
 }
