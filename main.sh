837d534843ca1df88513f38677aeaf805edf4aba
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 11:21:39 +0100

    feat: add short ram info

--- main.sh
+++ main.sh
@@ -11,32 +11,24 @@ set -euo pipefail
 # ---------- GETTERS ---------------------------------
 
 get_mem_info() {
-    local storage_terms=("RAM" "ROM" "SSD" "HDD" "CACHE" "SWAP")
     declare -A mem_info
 
     while (( $# )); do
-        for (( i=0; i < ${#storage_terms[@]}; i++ )); do
-            case $1 in
-                ${storage_terms[1]})
-                    full_ram_info=""
-                    short_ram_info=$(free -h | sed -n '/Mem/p' | awk '{ printf "total: %10s\nused: %11s\navailable: %6s\n", $2, $3, $7 }' | sed 's/Gi/Gb/')
-                    echo "$short_ram_info"
+        case $1 in
+            "${storage_terms[0]}")
+                full_ram_info=""
 
+                # process short ram info
+                short_ram_info="$(free -h | sed -n '/Mem/p' | 
+                    awk '{ printf "total: %10s\nused: %11s\navailable: %6s\n", $2, $3, $7 end}' | 
+                    sed 's/Gi/Gb/')"
+                echo "$short_ram_info"
+                
+                ;;
+            esac
+
+        shift
+    done
 }
 
-get_battery_info() {
-
-}
-
-get_cpu_info() {
-
-}
-
-get_gpu_info() {
-
-}
-
-#----------- INFO PROCESSORS -----------------
-info_processor() {
-    
-}
+get_mem_info RAM
