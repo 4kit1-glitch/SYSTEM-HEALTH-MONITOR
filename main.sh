0ab223d131ee8b6bc181419df7b64e40dab18832
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 15:26:25 +0100

    feat: add short cpu info

--- main.sh
+++ main.sh
@@ -31,16 +31,27 @@ get_mem_info() {
                 ;;
 
             "cache")
-                cache_info="$(free -h | sed -n '/Mem/p' | awk '{ printf "cached ram: %7s", $6}' | sed 's/Gi/Gb/g')"
+                # get cache info 
+                cache_info="$(free -h | sed -n '/Mem/p' | 
+                awk '{ printf "cached ram: %7s", $6}' | sed 's/Gi/Gb/g')"
                 ;;
 
             "secondary")
-                short_str_info=""
-                
+                short_str_info="$(lsblk -d -o NAME,SIZE,TYPE)"
+                echo $short_str_info
+                ;;
+            "cpu")
+                # short cpu info
+                short_cpu_info="$(lscpu | grep "Model name" |
+                awk '{ print $0 "\ngeneration: " $3 "\nprocessing speed: " $9 "\nmanufacturer: " $5$6 "\nversion: "$7}')"
+                echo $short_cpu_info
+                ;;
+            "gpu")
+            
                 ;;
             esac
         shift
     done
 }
 
-get_mem_info cache
+get_mem_info cpu
