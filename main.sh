c629fed9fa63f3af3ada3caa0d9048f15709e5f6
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 18:42:33 +0100

    add exported script dir so as to make script directory independent

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 
 #----------------------- GLOBAL VARS--------------------
-
+export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # ---------- GETTERS ---------------------------------
 
