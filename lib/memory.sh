96cdb9459887b77635926239b3564a2aa03c1174
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-23 19:22:36 +0100

    feat: add structure layout of memory

--- lib/memory.sh
+++ lib/memory.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
 
+# script carries functions that work with system memory info and test
+# ext stands for extended
+
 set -euo pipefail
+
+#----------- Getters --------------------------
+
+#---------------RAM-----------------------------
+# quick details about ram 
+get_short_ram_info() {
+
+}
+
+# full details about ram
+get_full_ram_info() {
+
+}
+
+#------------------ CACHE-------------------------
+
+# quick info on the cache
+get_cache_info() {
+
+}
+
+# deep cpu cache details
+get_ext_cache_info() {
+
+}
+
+# ---------------- processes --------------------------
+
+
