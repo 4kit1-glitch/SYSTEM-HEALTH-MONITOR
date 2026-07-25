7961818a34e39e12b7a0e2a7ac6692527c3d5a7d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-25 15:53:55 +0100

    feat: source all device scripts effectively

--- main.sh
+++ main.sh
@@ -9,11 +9,22 @@ set -euo pipefail
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
+export CORE_DIR="$SCRIPT_DIR/lib/core"
+export DEVICE_DIR="$SCRIPT_DIR/lib/devices"
+export FEATURE_DIR="$SCRIPT_DIR/features"
+
 # ----------------------- source scripts ---------------------
 
 # source core scripts
-for script in "$( ls lib/core)"; do
-    source "lib/core/$script"
+for script in $( ls "$CORE_DIR"); do
+    source "$CORE_DIR/$script"
 done
 
-echo $X
+# source device scripts
+for dir in $(ls "$DEVICE_DIR"); do 
+    for script in $( ls "$DEVICE_DIR/$dir"); do
+        source "$DEVICE_DIR/$dir/$script"
+    done
+done
+
+get_full_ram_info
