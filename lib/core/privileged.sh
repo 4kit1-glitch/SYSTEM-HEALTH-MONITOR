78a4e093c8b79980b21b1ab00135c68bf65cee1f
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-25 15:53:19 +0100

    fix: remove multiple sudo checks in one shell instance

--- lib/core/privileged.sh
+++ lib/core/privileged.sh
@@ -2,19 +2,27 @@
 
 # script performs a wrapper to sudo
 
+# the is_privileged var works with 1 or 0 -- 0 for no 1-255 for yes
+declare -i is_privileged=0
+
 # gives a process super user privileges
 run_privileged() {
-    if [[ $EUID -eq 0 ]]; then
-        "$@"
-        return $?
-    fi
     # control user decision
-    read -rp "Process requires super user privileges: proceed with sudo? [y/n]: " response
-    if [[ $response =~ ^[Yy]$ ]]; then 
+    if sudo -n true 2> /dev/null; then 
+        read -rp "Process requires super user privileges: proceed with sudo? [y/n]: " response
+
+        if [[ $response =~ ^[Yy]$ ]]; then
+            sudo "$@"
+            is_privileged=1
+            return $?
+        else 
+            printf "permission denied -- process aborted" >&2
+            is_privileged=0
+            return 1
+        fi
+    elif [[ ($is_privileged == 0) ]]; then
         sudo "$@"
-    else 
-        printf "permission denied -- process aborted" >&2
-        return 1
+        return $?
     fi
 }
 
