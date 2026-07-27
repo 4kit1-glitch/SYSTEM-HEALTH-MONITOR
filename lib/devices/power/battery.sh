c79a6add4ab6df170202ff3e428731f1a3c0f187
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-27 23:40:09 +0100

    feat: add power info

--- lib/devices/power/battery.sh
+++ lib/devices/power/battery.sh
@@ -4,12 +4,23 @@
 #---------- vars ------------
 declare -r POWER_SUPPLY_DIR="/sys/class/power_supply"
 
-declare -r ac_count=$(cat $POWER_SUPPLY_DIR/ADP* 2> /dev/null | wc -l)
-declare -r battery_count=$(cat $POWER_SUPPLY_DIR/BAT* 2> /dev/null | wc -l)
+declare -r ac_count=$(ls -ld $POWER_SUPPLY_DIR/ADP* 2> /dev/null | wc -l)
+declare -r battery_count=$(ls -ld $POWER_SUPPLY_DIR/BAT* 2> /dev/null | wc -l)
 
 
 #--------- getters ----------------
 
 get_battery_info(){
-    
+    printf "Power devices:/n"
+    printf "ac source num: %d\nnumber of batteries: %d" "$ac_count" "$battery_count"
+
+    for device in "$POWER_SUPPLY_DIR"; do
+        name=$(basename "$device")
+        type=%(cat "$dev/type" 2> /dev/null)
+        printf "%s : %s" "$name" "$type"
+
+        if [[ $type -eq "Baterry"]]; then
+            printf "Capacity: %s\%" "$(cat "$device/capacity")"
+            printf "Status: %s\%" "$(cat $device/status)"
+        fi
 }
