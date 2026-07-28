109fa615ed3e975e91bb439ba6940e12b522da23
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-28 22:29:38 +0100

    im dying bugssss

--- lib/devices/power/battery.sh
+++ lib/devices/power/battery.sh
@@ -11,23 +11,23 @@ declare -r battery_count=$(ls -ld $POWER_SUPPLY_DIR/BAT* 2> /dev/null | wc -l)
 #--------- getters ----------------
 
 get_battery_info(){
-    printf "Power devices:/n"
-    printf "ac source num: %d\nnumber of batteries: %d" "$ac_count" "$battery_count"
+    printf "Power devices:\n"
+    printf "ac source num: %d\nnumber of batteries: %d\n" "$ac_count" "$battery_count"
 
     for device in "$POWER_SUPPLY_DIR/*"; do
-        name=$(basename "$device")
-        type=$(cat "$dev/type" 2> /dev/null)
-        printf "%s : %s" "$name" "$type"
-
-        if [[ $type == "Baterry" ]]; then
+        echo $device
+        if [[ $type == "Baterry" ]]; then   # this has not been
             printf "Capacity: %s\%" "$(cat "$device/capacity")"
             printf "Status: %s\%" "$(cat "$device/status")"
             return 0
-        elif [[ $type == "Mains" ]]; then
+        elif [[ $type == "Mains" ]]; then   # show that source is alternating current
             printf "status: %s\%" "$(cat "$device/online")"
             return 0
         else 
             printf "files missing"
             return 1
         fi
+    done
 }
+
+get_battery_info
