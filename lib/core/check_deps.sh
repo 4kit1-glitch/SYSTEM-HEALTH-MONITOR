6189408c825a3d611a601a927a6ebceccde7b90e
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-29 23:42:17 +0100

    feat: add get dependencies function

--- lib/core/check_deps.sh
+++ lib/core/check_deps.sh
@@ -7,16 +7,19 @@ declare -r  required_deps=(
     "sed" "awk" "grep" "wc" "free"
     "dmidecode" "lscpu" "lsblk" "cat"
     "ls" "cd" "pwd" "dirname" "pactl"
-    "aplay"
+    "aplay" "lllllds" "dsjfjlsdk" "diowe"
 )
 
 declare -a missing_deps
+missing_count=0
 
 get_missing_deps() {
     # function gets missing dependencies
-    for dep in "${required_deps[@]}"; then
-        
-        
-        
+    for dep in "${required_deps[@]}"; do
+        if ! command -v "$dep" &> /dev/null; then
+            missing_deps[$missing_count]="$dep"
+            (( missing_count++ ))
+        fi
+    done
+    return $?
 }
-
