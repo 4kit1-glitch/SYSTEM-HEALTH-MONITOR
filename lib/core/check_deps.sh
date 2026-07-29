1b8134892f1cd3e2f1c235b7a8f25243c9693949
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-29 23:55:33 +0100

    refactor: change way missing dependency array is filled

--- lib/core/check_deps.sh
+++ lib/core/check_deps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# script check if program dependencies are installed
+
+
 # ---------------vars-------------------
 
 # array variable that stores the required dependencies of the program
@@ -7,19 +10,19 @@ declare -r  required_deps=(
     "sed" "awk" "grep" "wc" "free"
     "dmidecode" "lscpu" "lsblk" "cat"
     "ls" "cd" "pwd" "dirname" "pactl"
-    "aplay" "lllllds" "dsjfjlsdk" "diowe"
+    "aplay" "upower"
 )
+required_count=${#required_deps[@]}
 
-declare -a missing_deps
-missing_count=0
+declare -a missing_deps     # variable stores the missing dependencies
 
+#---------- getters -----------------
 get_missing_deps() {
     # function gets missing dependencies
     for dep in "${required_deps[@]}"; do
         if ! command -v "$dep" &> /dev/null; then
-            missing_deps[$missing_count]="$dep"
-            (( missing_count++ ))
+            missing_deps+=("$dep")
         fi
     done
-    return $?
+    [[${#missing_deps} -eq 0 ]] && return 0 || return 1
 }
