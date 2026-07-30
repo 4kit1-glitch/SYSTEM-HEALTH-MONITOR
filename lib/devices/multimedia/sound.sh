07fd8caeaf9ca3fbd1f2c0745f0f2fb9b45da65a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-30 18:16:43 +0100

    rename sound information sed file

--- lib/devices/multimedia/sound.sh
+++ lib/devices/multimedia/sound.sh
@@ -3,20 +3,26 @@
 
 # the programs used are presence are already verified in check_deps.sh
 #------------ vars ------------------------
-declare -r output_count=$(pactl list sinks short | wc -l)
-declare -r input_count=$(pactl list sources short | wc -l)
+declare -ri output_count=$(pactl list sinks short | wc -l)
+declare -ri input_count=$(pactl list sources short | wc -l)
 
 #---------------- Getters   -----------------
 
-get_sound_ouput_info() {
-    
+get_sound_output_info() {
+    printf "Sound output information\n"
+    printf "no of output sinks: %d\n" "$output_count"
+    sound_output_info=$(
+        pactl list sinks | sed -E -n -f $FEATURE_DIR/sound_info.sed | 
+        sed '/State/i\device'
+    )
+     printf "%s\n" "$sound_output_info"
 }
 
 get_sound_input_sources() {
-
+                                                                                                                                                                                                                                                                                                                                                                         
 }
 
 #------------ processes -----------------------
 get_audio_info_summary() {
-
+    echo p
 }
