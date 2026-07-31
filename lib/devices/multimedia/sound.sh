4b17e21d7b413f0aecf4abd9cadd3a3d983b330a
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-31 08:57:47 +0100

    fix: modify active device definintion to handle strings

--- lib/devices/multimedia/sound.sh
+++ lib/devices/multimedia/sound.sh
@@ -5,6 +5,9 @@
 #------------ vars ------------------------
 declare -ri output_count=$(pactl list sinks short | wc -l)
 declare -ri input_count=$(pactl list sources short | wc -l)
+declare -ri bluetooth_count=$(pactl list sinks short | grep "bluez" | wc -l)
+declare -r active_device="$(pactl list sinks short  | grep -Ei "running" |
+ awk '{printf "%s is active\n", $2} END{if( NR == 0) {printf "No audio device active\n"}')"
 
 #---------------- Getters   -----------------
 
@@ -19,10 +22,20 @@ get_sound_output_info() {
 }
 
 get_sound_input_sources() {
-                                                                                                                                                                                                                                                                                                                                                                         
+    printf "Sound input information\n"
+    printf "no of input sinks: %d\n" "$input_count"
+    sound_input_info=$(
+        pactl list sources | sed -E -n -f $FEATURE_DIR/sound_info.sed |
+        sed '/state/i\device'
+    )
+    printf "%s/n" "$sound_input_info"                                                                                                                                                                                                                                                                                                                                                  
 }
 
 #------------ processes -----------------------
 get_audio_info_summary() {
-    echo p
+    printf "output devices connected: %d\n" "$output_count"
+    printf "input devices connected: %d\n" "$input_count"
+    printf "bluetooth divices: %d\n" "$bluetooth_count"
+    printf "active device: %s\n" "$active_device" 
+    printf "volume: "
 }
