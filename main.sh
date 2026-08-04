fc9724be730fe58dd4d330f0049e8ff47946f95f
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-04 22:33:57 +0100

    feat: add program name variable

--- main.sh
+++ main.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # vim: noai:ts=4:sw=4:expandtab
 # shellcheck source=/dev/null
-# shellcheck disable=2034,2155
 #
 # Syskit: A system health monitor built in bash version 5.3+
 # https://github.com/4kit1-glitch/syskit
 #
 #
 
-readonly version=0.0.1
+# shellcheck disable=2034,2155
+readonly VERSION=0.0.1
+readonly PROGNAME=$(basename "$0")
 
-
-sys_locale=${LANG:-C}
+#sys_locale=${LANG:-C}
 
 # Speed up script by not using unicode.
 LC_ALL=C
@@ -19,11 +19,10 @@ LANG=C
 
 # at the moment config files will be stored in shm folder for testing purposes
 
+# shellcheck disable=2155
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${SCRIPT_DIR}/.config}
 PATH=$PATH:/usr/xpg4/bin:/usr/sbin:/sbin:/usr/etc:/usr/libexec
-
-shopt -s nocasematch
 
 set -euo pipefail
 
@@ -38,12 +37,17 @@ for script in "$CORE_DIR"/*.sh; do
     source "$script"
 done
 
-# checking program dependencies
-
-
 # source device scripts
 for dir in "$DEVICE_DIR"/*; do 
     for script in "$dir"/*.sh; do
         source "$script"
     done
 done
+
+# check and install missing program dependencies
+install_missing_deps
+confirm_installation
+
+main() {
+    echo pass
+}
