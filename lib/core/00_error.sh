acb1fafb1ae9af89801633dbab5c9d9a348921e0
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-05 01:58:52 +0100

    feat: add error codes in store

--- lib/core/00_error.sh
+++ lib/core/00_error.sh
@@ -3,3 +3,10 @@
 # script defines and stored error codes used in the program
 # code convension: ERR_WHAT_IT_REPRESENTS
 # make it as short as possible
+
+readonly ERR_SUCCESS
+readonly ERR_FAILURE
+readonly ERR_PERMISSION_DENIED
+readonly ERR_COMMAND_FAILED
+
+
