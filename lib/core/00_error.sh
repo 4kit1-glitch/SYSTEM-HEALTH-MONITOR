70c53d733202ea76108406f68c77e8e86207e6ea
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-05 13:36:15 +0100

    ignore shellcheck

--- lib/core/00_error.sh
+++ lib/core/00_error.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
+# shellcheck disable=2034
 
 # script defines and stored error codes used in the program
 # code convension: ERR_WHAT_IT_REPRESENTS
-# make it as short as possible
+# make it as short as possible    fi
 
 readonly ERR_SUCCESS=0  # operation successfule
 readonly ERR_FAILURE=1  # operation failed general 
