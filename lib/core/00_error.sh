88decaac5990206a6510f5b7ddd75d1f28a2bfd5
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-05 01:55:35 +0100

    feat: add error code store

--- lib/core/00_error.sh
+++ lib/core/00_error.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-# script defines and stored error codes used in the program 
+# script defines and stored error codes used in the program
+# code convension: ERR_WHAT_IT_REPRESENTS
+# make it as short as possible
