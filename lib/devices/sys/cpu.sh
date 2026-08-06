7605b4354750b2e10f81511b2dcecfd52719ed5d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-08-06 15:21:20 +0100

    fix: fix correct file name for load file info

--- lib/devices/sys/cpu.sh
+++ lib/devices/sys/cpu.sh
@@ -4,10 +4,18 @@
 # shellcheck source=/dev/null
 #
 
+# static info
 readonly CPU_INFO_FILE="/proc/cpuinfo"
+readonly LOAD_INFO_FILE="/proc/loadavg
+readonly THERMAL_INFO_FILE="/sys/class/thermal"
+
 
 get_cpu_model_info() {
-    local model_name=$(cat $CPU_INFO_FILE | grep -i "model name" | awk -F':' '{print $2}' | head -1)
+    [[ -f "$CPU_INFO_FILE" ]] && {
+        local model_name="$(cat $CPU_INFO_FILE | \
+        grep -i "model name" | \
+        awk -F':' '{print $2}' | head -1)"
+    }
     printf "%s" "$model_name"
 }
 
@@ -15,14 +23,17 @@ get_cpu_cores() {
     local core_count=$(cat $CPU_INFO_FILE | grep -ci "processor")
     printf "%s" "$core_count"
 }
-get_cpu_speed() {
 
-}
 get_cpu_usage() {
+    local total_time
+    local idle_time
+    local working_time
+    local usage_percent=$(bc -l <<< "scale=2 echo "100 * $working_time / $total_time"")
 
 }
 get_load_average() {
-
+    local load=$(cat $LOAD_INFO_FILE | awk '{print $1 $2 $3}')
+    printf "%s" "$load"
 }
 get_cpu_status() {
 
